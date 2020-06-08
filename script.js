e655a8c1b359e54743c7491ff02333896c0cefa4
Shela Duong <sheladuong@gmail.com>
2020-06-07 19:12:12 -0700

    Finalized Styling

--- script.js
+++ script.js
@@ -1,125 +1,82 @@
-const gameContainer = document.getElementById("game");
-let card1 = null;
-let card2 = null;
-let counter = 0;
-let noClicking = false;
+const splash = document.querySelector('.splash');
+document.addEventListener('DOMContentLoaded', (e) => {
+	setTimeout(() => {
+		splash.classList.add('display-none');
+	}, 3000);
+});
 
-const COLORS = [
-  "red",
-  "blue",
-  "green",
-  "orange",
-  "purple",
-  "red",
-  "blue",
-  "green",
-  "orange",
-  "purple"
-];
+let count = 0;
+const cards = document.querySelectorAll('.memory-card');
 
-// here is a helper function to shuffle an array
-// it returns the same array with values shuffled
-// it is based on an algorithm called Fisher Yates if you want to research more
-function shuffle(array) {
-  let counter = array.length;
-  // While there are elements in the array
-  while (counter > 0) {
-    // Pick a random index
-    let index = Math.floor(Math.random() * counter);
-    // Decrease counter by 1
-    counter--;
-    // And swap the last element with it
-    let temp = array[counter];
-    array[counter] = array[index];
-    array[index] = temp;
-  }
-  return array;
+let hasFlippedCard = false;
+let lockBoard = false;
+let firstCard, secondCard;
+
+function flipCard() {
+	if (lockBoard) return;
+	if (this === firstCard) return;
+
+	this.classList.add('flip');
+
+	if (!hasFlippedCard) {
+		hasFlippedCard = true;
+		firstCard = this;
+
+		return;
+	}
+
+	secondCard = this;
+	checkForMatch();
 }
 
-//THIS runs the shuffle function and stores it in shuffledColors 
-//WHEN DOM loads it'll run this 
-let shuffledColors = shuffle(COLORS);
+function checkForMatch() {
+	let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
 
+	if (isMatch) {
+		count += 2;
+	}
 
-
-
-// this function loops over the array of colors
-// it creates a new div and gives it a class with the value of the color
-// it also adds an event listener for a click for each card
-function createDivsForColors(colorArray) {
-  for (let color of colorArray) {
-    // create a new div
-    const newDiv = document.createElement("div");
-
-    // give it a class attribute for the value we are looping over
-    newDiv.classList.add(color);
-
-    // call a function handleCardClick when a div is clicked on
-    newDiv.addEventListener("click", handleCardClick);
-
-    // append the div to the element with an id of game
-    gameContainer.append(newDiv);
-  }
+	isMatch ? disableCards() : unflipCards();
 }
 
+function disableCards() {
+	firstCard.removeEventListener('click', flipCard);
+	secondCard.removeEventListener('click', flipCard);
 
-
-
-// TODO: Implement this function!
-function handleCardClick(event) {
-
-  if (noClicking) return;
-  if (event.target.classList.contains('flipped')) return;
-  // you can use event.target to see which element was clicked
-  // console.log("you just clicked", event.target);
-
-  let currentCard = event.target;
-  currentCard.style.backgroundColor = currentCard.classList[0];
-
-
-  if (!card1 || !card2) {
-    currentCard.classList.add('flipped');
-    card1 = card1 || currentCard;
-    card2 = currentCard === card1 ? null : currentCard;
-  }
-
-
-  if (card1 && card2) {
-    noClicking = true;
-    let gif1 = card1.classList[0];
-    let gif2 = card2.classList[0];
-
-
-    if (gif1 === gif2) {
-      counter += 2;
-      card1.removeEventListener("click", handleCardClick);
-      card2.removeEventListener("click", handleCardClick);
-      card1 = null;
-      card2 = null;
-      noClicking = false;
-
-    } else {
-      let matchTimer = setTimeout(function () {
-        //remove lipped and remove color 
-        card1.style.backgroundColor = '';
-        card2.style.backgroundColor = '';
-        card1.classList.remove('flipped');
-        card2.classList.remove('flipped');
-        card1 = null;
-        card2 = null;
-        noClicking = false;
-
-      }, 1000);
-
-    }
-
-  }
-
-  if (counter === COLORS.length) {
-    console.log('Game Over!');
-  }
-
+	resetBoard();
 }
 
-// when the DOM loads
-createDivsForColors(shuffledColors);
+function unflipCards() {
+	lockBoard = true;
+
+	setTimeout(() => {
+		firstCard.classList.remove('flip');
+		secondCard.classList.remove('flip');
+
+		resetBoard();
+	}, 1500);
+}
+
+function resetBoard() {
+	[ hasFlippedCard, lockBoard ] = [ false, false ];
+	[ firstCard, secondCard ] = [ null, null ];
+}
+
+(function shuffle() {
+	cards.forEach((card) => {
+		let randomPos = Math.floor(Math.random() * 12);
+		card.style.order = randomPos;
+	});
+})();
+
+cards.forEach((card) => card.addEventListener('click', flipCard));
+
+let restart = document.querySelector('button');
+
+restart.addEventListener('click', function() {
+	document.location.href = '';
+});
+
+if (count === 12) {
+	alert('You Win!');
+}
